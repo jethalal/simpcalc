143b5a7b833d7384a0a2ef74d049b07272303383
jethalal <hohafa123@gmail.com>
2015-10-28 18:41:46 -0700

    Create calc.java

---
+++ calc.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class samosa;
+public static void main(String args[]){
+  System.out.println("enter 1num");
+  int num1;
+  System.out.println("enter 2nd num");
+  int num2;
+  System.out.println("the answer is" num1 + num2 ");
+}
